a362432ce5a3568397a903eaa6398dc5c0db8b75
TJ Holowaychuk <tj@vision-media.ca>
2017-07-02 17:46:34 -0700

    add quick cli. Closes #2

---
+++ cmd/polls/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/tj/kingpin"
+)
+
+// version of polls.
+var version = "0.0.1"
+
+// endpoint for polls.
+var endpoint = "https://m131jyck4m.execute-api.us-west-2.amazonaws.com/prod"
+
+// TODO: move this stuff to some client lib.
+type input struct {
+	Options []string `json:"body"`
+}
+
+type output struct {
+	ID string `json:"id"`
+}
+
+func main() {
+	app := kingpin.New("polls", "GitHub polls.")
+	app.Version(version)
+
+	create := app.Command("new", "Create a new poll.")
+	options := create.Arg("options", "Poll options.").Required().Strings()
+	create.Example(`polls new Tobi Loki Jane`, "Create a new poll for who is the best ferret.")
+	create.Example(`polls new "Cats are better" "Ferrets are better"`, "Create a new poll with larger options.")
+
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	case create.FullCommand():
+		b, err := json.Marshal(input{Options: *options})
+		if err != nil {
+			log.Fatalf("error marshaling: %s", err)
+		}
+
+		res, err := http.Post(endpoint+"/poll", "application/json", bytes.NewReader(b))
+		if err != nil {
+			log.Fatalf("error requesting: %s", err)
+		}
+		defer res.Body.Close()
+
+		var out output
+		if err := json.NewDecoder(res.Body).Decode(&out); err != nil {
+			log.Fatalf("error unmarshaling resonse: %s", err)
+		}
+
+		fmt.Println(out.ID)
+	}
+}
