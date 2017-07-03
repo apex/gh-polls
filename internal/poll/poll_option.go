427e69b4e12c1d0cf6d4163901d1fb081db3780a
TJ Holowaychuk <tj@vision-media.ca>
2017-07-02 20:50:59 -0700

    change svg height to be slightly slimmer

--- internal/poll/poll_option.go
+++ internal/poll/poll_option.go
@@ -11,10 +11,10 @@ import (
 var fontFamily = `-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif`
 
 // option svg.
-var option = `<svg width="448px" height="62px" viewBox="0 0 448 62" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+var option = `<svg width="448px" height="58px" viewBox="0 0 448 58" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="poll">
-            <g id="Group" transform="translate(17.000000, 14.000000)">
+            <g id="Group" transform="translate(17.000000, 10.000000)">
                 <rect id="Rectangle" fill="#F1F3F5" x="0" y="19" width="334" height="14" rx="2"></rect>
                 <rect id="Rectangle" fill="#7950F2" x="0" y="19" width="{{.Width}}" height="14" rx="2"></rect>
                 <text id="100%" font-family="{{.FontFamily}}" font-size="12" font-weight="normal" letter-spacing="1.857333" fill="#212529">
